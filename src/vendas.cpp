ca319e88277937a20af725cd676ee11ef17903f5
Joao Prado <joaopradocampos@gmail.com>
2025-06-20 18:15:25 -0300

    Início da função de adicionar itens.

--- src/vendas.cpp
+++ src/vendas.cpp
@@ -9,9 +9,7 @@
 using namespace std;
 
 Vendas::Vendas(int codigoP, int quant) {
-    codigoProduto = codigoP;
-    quantidade = quant;
-    setCodigoVenda();
+    verificar_estoque(codigoP,quant);
 }
 void Vendas::setCodigoVenda() {
     unsigned seed = time(0);
@@ -19,10 +17,33 @@ void Vendas::setCodigoVenda() {
     // Criar verificação (depende da formatação do txt)
     codigoVenda = rand() % 1001;
 }
-void Vendas::realizar_compra(int codigo, int quant) {
-    // Implementação do
-    // Produtos
-    // int estoque =
+
+void Vendas::aicionar_item(int codigoItem,int quant) {
+    Produtos item = Produtos::buscar_codigo_produto(codigoItem);
+    ItemVenda produtoSolicitado;
+    produtoSolicitado.codigoProduto = codigoItem;
+    produtoSolicitado.nomeProduto = item.nome;
+    produtoSolicitado.precoUnitario = item.preco;
+    if (verificar_estoque(codigoItem,quant)) {
+        produtoSolicitado.quantidadeVendida = quant;
+        produtoSolicitado.precoTotal = item.preco * quant;
+    }
+
+
+}
+bool Vendas::verificar_estoque(int codigoP, int quant) {
+    Produtos produtoSolicitado = getProduto(codigoP);
+    if (quant<=produtoSolicitado.estoque) {
+        produtoSolicitado-quant;
+        quantidade = quant;
+        calcular_preco(quant,produtoSolicitado);
+    }
+
+}
+void Vendas::calcular_preco(int quant, Produtos produtoSolicitado) {
+    float precoU = produtoSolicitado.getPreco();
+    precoUnidade = precoU;
+    precoTotal = precoU*quant;
 }
 
 int main() {
--- src/vendas.h
+++ src/vendas.h
@@ -1,19 +1,33 @@
 #ifndef VENDAS_H
 #define VENDAS_H
+#include <string>
+#include <vector>
+using namespace std;
+
+struct ItemVenda{
+    int codigoProduto;
+    string nomeProduto;
+    int quantidadeVendida;
+    float precoUnitario;
+    float precoTotal;};
 
 class Vendas {
-private:
-    int codigoVenda;
-    int codigoProduto;
-    int quantidade;
-    float precoUnidade;
-    float precoTotal;
-    float precoComFrete;
+    private:
+        int codigoVenda;
+        string nomeComprador;
+        float valorTotal;
+        float frete;
+        vector<ItemVenda> itens;
 
-public:
-    Vendas(int codigoP, int quant);
-    void setCodigoVenda();
-    void realizar_compra(int codigo, int quant);
+    public:
+        Vendas(int codigoP, int quant);
+        void setCodigoVenda();
+
+        void aicionar_item(int codigoItem, int quant);
+
+        void calcular_preco(int quant,Produtos produtoSolicitado);
+
+        bool verificar_estoque(int codigo, int quant);
 };
 
 #endif
